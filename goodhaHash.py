3ac51a97cdd9d636aecbf8ace86294324844b664
AleksandarDamnjanovic <aor.aleksandar@gmail.com>
2021-12-29 17:15:02 +0100

    Updated

--- goodhaHash.py
+++ goodhaHash.py
@@ -2,64 +2,168 @@
 #Title: Goodha Hash
 #Author: Aleksandar Damnjanovic
 #Date: 27.11.2021
+#Last edit: 29.12.2021
 #*****************************
 
 import math
 import string
+import struct
 
 class GoodhaHash:
 
     @staticmethod
-    def hash(input):
+    def hash(input, type='hex'):
 
-        if isinstance(input, int):
+        if type=='hex':
             input= hex(input)
             input=input[2:len(input)]
 
-        if isinstance(input,str):
+        if type=='string':
             input= input.encode().hex()
 
-        if isinstance(input, bytes):
+        if type=='bytes':
             input= input.hex()
 
-        if isinstance(input, float):
+        if type=='float':
             input= str(input).encode().hex()
 
+        value=''
         if len(input)<256:
             value= GoodhaHash._spread(input)
         else:
             value= GoodhaHash._cut(input)
             value= GoodhaHash._spread(value)
 
-        value= GoodhaHash._root(value)
-        value= GoodhaHash._shift(value)
         value= GoodhaHash._cut(value)
-        value= GoodhaHash._spread(value)
+        if len(value)<256:
+            value= GoodhaHash._spread(value)
+            value= GoodhaHash._cut(value)
+        
         value= GoodhaHash._shift(value)
+        value= GoodhaHash._mess(value)
         value= GoodhaHash._cut(value)
+        value= GoodhaHash._shift(value)
 
         return '0x'+value
 
     @staticmethod
     def _spread(input):
-        count=0
-        r=''
+        r=input
         if input=='':
             for i in range(256):
                 res=i & 0xff
                 res= hex(res)
                 r+=res[2:]
         else:
-            for i in range(256):
-                e=input[count]
-                count+=1
-                if count>len(input)-1:
-                    count=0
-
-                e=e.encode().hex()
-                r+=e
+            for i in range(256-len(input)):
+                e='0'
+                r=e+r
 
         return r
+
+    @staticmethod
+    def _mess(input):
+        d=list()
+        for i in range(0,len(input),16):
+            d.append(int(input[i:i+16],16))
+
+        if len(d)==7:
+            d.append(d[2])
+
+        if len(d)==6:
+            d.append(d[3])
+            d.append(d[1])   
+
+        for i in range(8):
+            d[i]+=d[0]
+
+        for i in range(8):
+            d[i]*=d[1]
+
+        for i in range(8):
+            d[i]= d[i] - d[2]
+
+        for i in range(8):
+            d[i]-=d[4]
+
+        for i in range(8):
+            d[i]|=d[5]
+
+        for i in range(8):
+            d[i]^=d[6]
+
+        for i in range(8):
+            d[i]&=d[7]
+
+        value=''
+        for i in range(len(d)):
+            vv=hex(d[i])
+            value+=vv[2:]
+
+        return value
+
+    @staticmethod
+    def _shift(input):
+        d=list()
+        for i in range(0,len(input),16):
+            d.append(int(input[i:i+16],16))
+
+        for i in range(len(d)):
+            d[i]=d[i]<<3|5>>d[i]
+
+        value=''
+        for i in range(len(d)):
+            vv=hex(d[i])
+            value+=vv[2:]
+
+        return value    
+        
+
+    @staticmethod
+    def _cut(input):
+        l=list()
+        d=[0x0000000000000000]*8
+        for i in range(0,len(input),16):
+            l.append(input[i:i+16])
+
+        count=0
+        for i in range(len(l)):
+            d[count]= d[count]+int(l[i],16)
+            count+=1
+            if count==8:
+                count=0
+        
+        d1=[0]*8
+        for i in range(len(d)):
+            d1[i]=d[i]<<4|4>>d[i]
+        
+        for i in range(len(d)):
+            d[i]=d[i]^d1[i]
+        
+        count=0
+        for i in range(len(d)):
+            d[i]=d[i]<<count|(8-count)>>d[i]
+            count+=1
+
+        count=8
+        for i in range(len(d1)):
+            d1[i]=d1[i]<<count|(8-count)>>d1[i]
+            count-=1
+
+        d2=[0]*8
+        for i in range(len(d2)):
+            d2[i]=d[i]|d1[i]
+
+        for i in range(len(d)):
+            vv=(d2[i]*d1[i])^(d2[i]-d1[i])
+            d[i]=d[i]^vv
+
+        value=''
+        for i in range(len(d)):
+            vv=hex(d[i])
+            value+=vv[2:]
+
+        return value
 
     @staticmethod
     def _shift(input):
@@ -74,63 +178,4 @@ class GoodhaHash:
                 if(count==5):
                     count=0
                 ret+=hex(e)[2:]
-        return ret
-
-    @staticmethod
-    def _cut(input):
-        ret=''
-        count=0
-        row=[0x00]*64
-        for i in range(0,len(input),2):
-            e=input[i:i+2]
-            e=int(e,16)
-            e= e & 0xff
-            if count % 7==0:
-                row[count]=row[count]-e
-            elif count % 5==0:
-                row[count]=row[count]+e
-            elif count % 3==0:
-                row[count]=row[count]*e
-            else:
-                row[count]=row[count]+(e<<i%3)|(e>>(5))
-            count+=1
-            if count==64:
-                count=0
-
-        for i in range(64):
-            e= row[i]
-            e= e & 0xff
-
-            if e==0x00:
-                e='0x00'
-            else:
-                e=hex(e)
-
-            e=e[2:]
-
-            if(len(e)==1):
-                e='0'+e
-
-            ret+=e
-
-        return ret
-
-    @staticmethod
-    def _root(input):
-        l=[]
-        for i in range(0, len(input), 8):
-            e= input[i:i+8]
-            e=int(e,16)
-            r=math.sqrt(e)
-            f=int(e)
-            p=r-f
-            p=p*100000000000
-            e=e*f
-            e=e^int(p)
-            e=hex(e)
-            e=e.replace("-",'')
-            e=e[2:]
-            l.append(e)
-
-        ret= l[15]+l[13]+l[11]+l[9]+l[7]+l[5]+l[3]+l[1]+l[14]+l[12]+l[10]+l[8]+l[6]+l[0]+l[2]+l[4]
         return ret
